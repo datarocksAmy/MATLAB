015b9063fe189d48469177eda66e68428f7b5dae
datarocksAmy <clthf@mail.umkc.edu>
2017-10-31 22:07:17 -0500

    comment update

--- Radial Basis Function (Exact & Regular)/parseData.m
+++ Radial Basis Function (Exact & Regular)/parseData.m
@@ -1,5 +1,8 @@
+% Function for labeling Malignant and Benign as 1 if classify correctly
+
 function [modify_output, modify_T] = parseData(original_output, original_T)
 
+% Get total row number
 %colNum = size(Reg_test_output, 1);
 rowNum = size(original_output, 2);
 
@@ -10,6 +13,7 @@ modify_T = zeros(2, rowNum);
 % Initialize row from 1
 row = 1;
 
+% Start parsing the data in each row
 while row <= rowNum
     % Split into two row mark as 1(yes) and 0(no) for Malignant & Benign
     if (original_output(1, row) == mode(original_output))
@@ -25,6 +29,7 @@ while row <= rowNum
         modify_T(2, row) = 1; % Malignant
     end
     
+    % Increment row by 1
     row = row + 1;
 end
 end
