fbd4248069308ff2f989452088542ad1731d34d9
datarocksAmy <clthf@mail.umkc.edu>
2017-11-14 14:35:24 -0600

    Add function

--- Gesture Recognition/SubjectSpecific_3CV.m
+++ Gesture Recognition/SubjectSpecific_3CV.m
@@ -4,14 +4,23 @@
 % -------------------------------------------------------------------
 
 % Clear memory data
-clc
-clear 
-close all
+%clc
+%clear 
+%close all
 
 % Load 3 Rounds of x, z of each movement for each Person
-[ P1_C, P1_T, P1_R, P1_D ] = Encapsulate('GestureData\Person1.xls');
-[ P2_C, P2_T, P2_R, P2_D ] = Encapsulate('GestureData\Person2.xls');
-[ P3_C, P3_T, P3_R, P3_D ]  = Encapsulate('GestureData\Person3.xls');
-[ P4_C, P4_T, P4_R, P4_D ]  = Encapsulate('GestureData\Person4.xls');
-[ P5_C, P5_T, P5_R, P5_D ]  = Encapsulate('GestureData\Person5.xls');
-[ P6_C, P6_T, P6_R, P6_D ]  = Encapsulate('GestureData\Person6.xls');
+[ P_C, P_T, P_R, P_D ] = Encapsulate('GestureData\Person1.xls');
+%[ P2_C, P2_T, P2_R, P2_D ] = Encapsulate('GestureData\Person2.xls');
+%[ P3_C, P3_T, P3_R, P3_D ]  = Encapsulate('GestureData\Person3.xls');
+%[ P4_C, P4_T, P4_R, P4_D ]  = Encapsulate('GestureData\Person4.xls');
+%[ P5_C, P5_T, P5_R, P5_D ]  = Encapsulate('GestureData\Person5.xls');
+%[ P6_C, P6_T, P6_R, P6_D ]  = Encapsulate('GestureData\Person6.xls');
+
+% 1-D Time Delay Nerual Network ( 1 Hidden Layer )
+TDNNnet = timedelaynet(0:15, 1, 'trainbr');
+TDNNnet.divideFcn = '';
+TDNN.layer{2}.transferFcn = 'transig';
+TDNN.divideParam.transRatio = 1;
+TDNN.divideParam.valRatio = 0;
+TDNN.divideParam.testRatio = ;
+
--- Gesture Recognition/movementTarget.m
+++ Gesture Recognition/movementTarget.m
@@ -1,22 +1,31 @@
 % One Hot Target for all 4 Movements
-function[ stackMovementT ] = movementTarget()
+function[ stackMovementTrain, stackMovementTest ] = movementTarget(foldType)
 
 % Vectors for 1s ( is Class ) and -1s ( Not Class ) 
 isClass = ones(1,300);
 notClass = -ones(1,300);
 
+% Target for 3 CV
+if strcmp(foldType, '3 Fold') == 1
 % Circle
-CTarget = [isClass, notClass, notClass, notClass];
+CTarget_Train = [isClass, notClass, notClass, notClass, isClass, notClass, notClass, notClass];
+CTarget_Test = [isClass, notClass, notClass, notClass];
 
 % Triangle
-TTarget = [notClass, isClass, notClass, notClass];
+TTarget_Train = [notClass, isClass, notClass, notClass, notClass, isClass, notClass, notClass];
+TTarget_Test = [notClass, isClass, notClass, notClass];
 
 % Right
-RTarget = [notClass, notClass, isClass, notClass];
+RTarget_Train = [notClass, notClass, isClass, notClass, notClass, notClass, isClass, notClass];
+RTarget_Test = [notClass, notClass, isClass, notClass];
 
 % Down
-DTarget = [notClass, notClass, notClass, isClass];
+DTarget_Train = [notClass, notClass, notClass, isClass, notClass, notClass, notClass, isClass];
+DTarget_Test = [notClass, notClass, notClass, isClass];
 
-% Stack all 4 movements of targets 
-stackMovementT = [CTarget; TTarget; RTarget; DTarget];
+% Stack all 4 movements of targets for Training and Testing
+stackMovementTrain = [CTarget_Train; TTarget_Train; RTarget_Train; DTarget_Train];
+stackMovementTest = [CTarget_Test; TTarget_Test; RTarget_Test; DTarget_Test];
 
+end
+end
