9e7c004395be4c7557f374ac059fc49f0550bb04
datarocksAmy <clthf@mail.umkc.edu>
2017-11-17 15:24:44 -0600

    serialize function

--- Gesture Recognition/Subject Independent-6CV/serializePersonXZ.m
+++ Gesture Recognition/Subject Independent-6CV/serializePersonXZ.m
@@ -1,24 +1,25 @@
-% Encapsulate all data into 2 matrices : x and z per Person
+% Serialize all three rounds of data into 2 rows : x and z for each person
 
-function [P_xz] = EncapsulatePerson(fileName)
+function [P_xz] = serializePersonXZ(fileName)
 
 % Circle
 [R1C_x, R1C_z, R2C_x, R2C_z, R3C_x, R3C_z] = combExcelData(fileName, 'Circle');
-circle_xz = [R1C_x,  R1C_z, R2C_x, R2C_z, R3C_x, R3C_z]';
+circle_xz = [R1C_x,  R1C_z; R2C_x, R2C_z; R3C_x, R3C_z]';
 
 % Triangle
 [R1T_x, R1T_z, R2T_x, R2T_z, R3T_x, R3T_z] = combExcelData(fileName, 'Triangle');
-triangle_xz = [R1T_x,  R1T_z, R2T_x, R2T_z, R3T_x, R3T_z]';
+triangle_xz = [R1T_x,  R1T_z; R2T_x, R2T_z; R3T_x, R3T_z]';
 
 % Right
 [R1R_x, R1R_z, R2R_x, R2R_z, R3R_x, R3R_z] = combExcelData(fileName, 'Right');
-right_xz = [R1R_x, R1R_z, R2R_x, R2R_z, R3R_x, R3R_z]';
+right_xz = [R1R_x, R1R_z; R2R_x, R2R_z; R3R_x, R3R_z]';
 
 % Down
 [R1D_x, R1D_z, R2D_x, R2D_z, R3D_x, R3D_z] = combExcelData(fileName, 'Down');
-down_xz = [R1D_x, R1D_z, R2D_x, R2D_z, R3D_x, R3D_z]';
+down_xz = [R1D_x, R1D_z; R2D_x, R2D_z; R3D_x, R3D_z]';
 
 % Stack up 4 movements for Person's x,z
 P_xz = [circle_xz, triangle_xz,  right_xz, down_xz];
 
 end
+
