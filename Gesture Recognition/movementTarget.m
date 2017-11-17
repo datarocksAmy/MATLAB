49f4697d027878fd7895fd803faade32e20ca278
datarocksAmy <clthf@mail.umkc.edu>
2017-11-16 20:46:01 -0600

    function added

--- Gesture Recognition/movementTarget.m
+++ Gesture Recognition/movementTarget.m
@@ -23,9 +23,38 @@ RTarget_Test = [notClass, notClass, isClass, notClass];
 DTarget_Train = [notClass, notClass, notClass, isClass, notClass, notClass, notClass, isClass];
 DTarget_Test = [notClass, notClass, notClass, isClass];
 
+% Target for 6 CV
+elseif strcmp(foldType, '6 Fold') == 1
+% Circle
+C = [isClass, notClass, notClass, notClass];
+CTarget_Train = repmat(C, 1, 15);
+CTarget_Test = repmat(C, 1, 3);
+
+% Triangle
+T = [notClass, isClass, notClass, notClass];
+TTarget_Train = repmat(T, 1, 15);
+TTarget_Test = repmat(T, 1, 3);
+
+% Right
+R = [notClass, notClass, isClass, notClass];
+RTarget_Train =repmat(R, 1, 15);
+RTarget_Test = repmat(R, 1, 3);
+
+% Down
+D = [notClass, notClass, notClass, isClass];
+DTarget_Train = repmat(D, 1, 15);
+DTarget_Test = repmat(D, 1, 3);    
+
 % Stack all 4 movements of targets for Training and Testing
 stackMovementTrain = [CTarget_Train; TTarget_Train; RTarget_Train; DTarget_Train];
 stackMovementTest = [CTarget_Test; TTarget_Test; RTarget_Test; DTarget_Test];
+
+% Target for Prefold
+elseif strcmp(foldType, 'Prefold') == 1
+preTarget = [isClass, notClass, notClass, notClass];    
+
+stackMovementTrain = preTarget;
+stackMovementTest = preTarget;
 
 end
 end
