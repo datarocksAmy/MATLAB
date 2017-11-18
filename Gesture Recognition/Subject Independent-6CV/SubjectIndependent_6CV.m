94c4ebb5df5bb228e928bab3547081b8a14fb1d8
datarocksAmy <clthf@mail.umkc.edu>
2017-11-17 21:54:44 -0600

    6cv main

--- Gesture Recognition/Subject Independent-6CV/SubjectIndependent_6CV.m
+++ Gesture Recognition/Subject Independent-6CV/SubjectIndependent_6CV.m
@@ -9,22 +9,19 @@ clear
 close all
 
 % Load 3 Rounds of x, z of each movement for each Person
-[ P1_xz ] = EncapsulatePerson('GestureData\Person1.xls');
-[ P2_xz ] = EncapsulatePerson('GestureData\Person2.xls');
-[ P3_xz]  = EncapsulatePerson('GestureData\Person3.xls');
-[ P4_xz ]  = EncapsulatePerson('GestureData\Person4.xls');
-[ P5_xz ]  = EncapsulatePerson('GestureData\Person5.xls');
-[ P6_xz ]  = EncapsulatePerson('GestureData\Person6.xls');
+[ P1_xz ] = serializePersonXZ('GestureData\Person1.xls');
+[ P2_xz ] = serializePersonXZ('GestureData\Person2.xls');
+[ P3_xz]  = serializePersonXZ('GestureData\Person3.xls');
+[ P4_xz ]  = serializePersonXZ('GestureData\Person4.xls');
+[ P5_xz ]  = serializePersonXZ('GestureData\Person5.xls');
+[ P6_xz ]  = serializePersonXZ('GestureData\Person6.xls');
 
-
-% 1-D Time Delay Nerual Network ( 1 Hidden Layer )
-TDNNnet = timedelaynet(1:30, 1, 'trainbr');
-TDNNnet.layers{2}.transferFcn = 'tansig';
-TDNNnet.divideParam.trainRatio = 1;
-TDNNnet.divideParam.valRatio = 0;
-TDNNnet.divideParam.testRatio = 0;
-TDNNnet.trainParam.epochs = 100;
+% Load Time Delay Nerual Network
+load('Depth15NodeNum30_Net.mat');
 
 % 6 Fold Cross Validation
 [ trained6CVnet, predictTestT1, predictTestT2, predictTestT3, predictTestT4, predictTestT5, predictTestT6, stackMovementTest] ...
                                                                                                                                     = sixFoldCV(TDNNnet, P1_xz, P2_xz, P3_xz,P4_xz,P5_xz,P6_xz);
+                                                                                                                                
+ % Plot ROC for CV1, CV2, CV3 and overall CV + save all graphs
+confusionROCSixF(predictTestT1, predictTestT2, predictTestT3, predictTestT4, predictTestT5, predictTestT6, stackMovementTest);
