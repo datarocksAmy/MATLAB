e6ab5d09cdf1d00a092d13e5d1c69ea3b999fbe2
datarocksAmy <clthf@mail.umkc.edu>
2017-12-03 20:18:20 -0600

    Add Label

--- Facial Recognition/AutoEncoder Part A/imageCategory.m
+++ Facial Recognition/AutoEncoder Part A/imageCategory.m
@@ -1,31 +1,36 @@
 % Load in images to create image vectos for testing and training / each person
 
-function [ train_img_vec, test_img_vec ] = imageCategory(fileName)
+function [ train_img_vec, train_Label, test_img_vec, test_Label ] = imageCategory(faceNum)
 
 % Read in image values for each person
-img1= imread(strcat('faceImg\', fileName, '\1.pgm'));
-img2= imread(strcat('faceImg\', fileName, '\2.pgm'));
-img3= imread(strcat('faceImg\', fileName, '\3.pgm'));
-img4= imread(strcat('faceImg\', fileName, '\4.pgm'));
-img5= imread(strcat('faceImg\', fileName, '\5.pgm'));
-img6= imread(strcat('faceImg\', fileName, '\6.pgm'));
-img7= imread(strcat('faceImg\', fileName, '\7.pgm'));
-img8= imread(strcat('faceImg\', fileName, '\8.pgm'));
-img9= imread(strcat('faceImg\', fileName, '\9.pgm'));
-img10= imread(strcat('faceImg\', fileName, '\10.pgm'));
+faceNumM = int2str(faceNum);
+img1= imread(strcat('faceImg\s', faceNumM, '\1.pgm'));
+img2= imread(strcat('faceImg\s', faceNumM, '\2.pgm'));
+img3= imread(strcat('faceImg\s', faceNumM, '\3.pgm'));
+img4= imread(strcat('faceImg\s', faceNumM, '\4.pgm'));
+img5= imread(strcat('faceImg\s', faceNumM, '\5.pgm'));
+img6= imread(strcat('faceImg\s', faceNumM, '\6.pgm'));
+img7= imread(strcat('faceImg\s', faceNumM, '\7.pgm'));
+img8= imread(strcat('faceImg\s', faceNumM, '\8.pgm'));
+img9= imread(strcat('faceImg\s', faceNumM, '\9.pgm'));
+img10= imread(strcat('faceImg\s', faceNumM, '\10.pgm'));
 
 % Randomly pick 6 for training and 4 for testing
-all = (1:10);
+data_all = (1:10);
 trainDataPick = randperm(10,6);
-testDataPick = setdiff(all, trainDataPick);
+testDataPick = setdiff(data_all, trainDataPick);
 
 % Grab the images for training picks and convert each image into cell array
 % representation
 train_img_vec = [{eval(strcat('img', int2str(trainDataPick(1))))} {eval(strcat('img', int2str(trainDataPick(2))))} {eval(strcat('img', int2str(trainDataPick(3))))} ...
 {eval(strcat('img', int2str(trainDataPick(4))))} {eval(strcat('img', int2str(trainDataPick(5))))} {eval(strcat('img', int2str(trainDataPick(6))))}];
 
+% Train Label for each category
+train_Label(1, 1:6) = faceNum;
+
 % Same for testing picks 
 test_img_vec = [{eval(strcat('img', int2str(trainDataPick(1))))} {eval(strcat('img', int2str(trainDataPick(2))))} {eval(strcat('img', int2str(trainDataPick(3))))} ...
 {eval(strcat('img', int2str(trainDataPick(4))))}];
+test_Label(1, 1:4) = faceNum;
 
 end
